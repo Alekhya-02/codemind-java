c373f7245e347906c9e7c8b2c7a3bdcf9cc2966f
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-04-07 21:35:29 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int n,m,mul;         Scanner buf = new Scanner(System.in);         n = buf.nextInt();         m = buf.nextInt();         mul=n*m;         System.out.println(mul);
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,m,mul;
+        Scanner buf = new Scanner(System.in);
+        n = buf.nextInt();
+        m = buf.nextInt();
+        mul=n*m;
+        System.out.println(mul);
+        
+    }
+}
