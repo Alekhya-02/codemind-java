c7182a751f10d0a92829caa489824e28d002f063
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-04-07 21:36:28 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int n,m,div;         Scanner buf = new Scanner(System.in);         n = buf.nextInt();         m = buf.nextInt();         div=n/m;         System.out.println(div);
        }
    }

---
+++ Division.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,m,div;
+        Scanner buf = new Scanner(System.in);
+        n = buf.nextInt();
+        m = buf.nextInt();
+        div=n/m;
+        System.out.println(div);
+        
+    }
+}
