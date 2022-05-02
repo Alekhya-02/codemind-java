3139f9dc6802d76cc61850399f10b344ef8a1093
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2022-05-02 11:19:32 +0530

    import java.util.Scanner; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,cnt=0,sum=0,b=0;         int avg;         n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {            a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(a[i]%2!=0)             {                 b=i;             }         }         System.out.println(b);     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,cnt=0,sum=0,b=0;
+        int avg;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+           a[i]=sc.nextInt(); 
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2!=0)
+            {
+                b=i;
+            }
+        }
+        System.out.println(b);
+    }
+}
